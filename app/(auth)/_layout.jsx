014b6e73cae912802d74ca9cc3e8eff6b0162d17
Alexandre <67651570+alexandregr26@users.noreply.github.com>
2024-05-17 10:34:39 -0400

    Auth Screens

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,13 +1,30 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
-// Only the Auth's screens and onboarding do not have navigation footer. The other screens do. This is why wer are using a separate layout.
+// Only the Auth's screens and onboarding do not have navigation footer. The other screens do. This is why we are using a separate layout.
 
 const AuthLayout = () => {
     return (
-        <View>
-            <Text>AuthLayout</Text>
-        </View>
+        <>
+            <Stack>
+                <Stack.Screen
+                    name="sign-in"
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen
+                    name="sign-up"
+                    options={{
+                        headerShown: false
+                    }}
+                />
+            </Stack>
+
+            <StatusBar backgroundColor='#161622' style='light' />
+        </>
     )
 }
 
