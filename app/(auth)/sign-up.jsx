c55d790af7325596597fce1bcad78ed5453f464a
Alexandre <67651570+alexandregr26@users.noreply.github.com>
2024-05-17 18:38:31 -0400

    Sign Up + Sign In Logic

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
 
 const SignUp = () => {
     const [form, setForm] = useState({
@@ -15,8 +16,24 @@ const SignUp = () => {
 
     const [isSubmitting, setisSubmitting] = useState(false)
 
-    const submit = () => {
+    const submit = async () => {
+        if (!form.username || !form.email || !form.password) {
+            Alert.alert('Error', 'Please fill in all the fields')
+        }
 
+        setisSubmitting(true);
+
+        try {
+            const result = await createUser(form.email, form.password, form.username);
+
+            // set it to global state...
+
+            router.replace('/home')
+        } catch (error) {
+            Alert.alert('Error', error.message)
+        } finally {
+            setisSubmitting(false)
+        }
     }
 
     return (
@@ -45,7 +62,7 @@ const SignUp = () => {
                         otherStyles="mt-7"
                     />
                     <CustomButton
-                        title="Sign In"
+                        title="Sign Up"
                         handlePress={submit}
                         containerStyles="mt-7"
                         isLoading={isSubmitting}
